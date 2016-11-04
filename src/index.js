77166bcc778c89b5a6d7dcbba50fccdc06869d6f
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-11-04 13:38:06 -0500

    setup material-ui

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import {render} from 'react-dom'
+import App from './components/App'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+import './index.css'
+import light from './theme'
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin()
+
+const Main = () => (
+  <MuiThemeProvider muiTheme={getMuiTheme(light)}>
+    <App/>
+  </MuiThemeProvider>
 )
+
+render(<Main/>, document.getElementById('root'))
